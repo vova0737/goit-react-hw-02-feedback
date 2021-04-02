0edc34537099806b927797bead9642ef89953836
Volodymyr Bondar <vova0737@gmail.com>
2021-04-02 23:48:00 +0300

    comps default export add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { Component } from 'react';
-import Section from './components/Section/Section';
-import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
-import Statistics from './components/Statistics/Statistics';
-import Notification from './components/Notification/Notification';
+import Section from './components/Section';
+import Feedback from './components/Feedback';
+import Statistics from './components/Statistics';
+import Notification from './components/Notification';
 
